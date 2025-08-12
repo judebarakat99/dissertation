8acf17c5466215e33e6868707a2a26acdf0b8ef6
judebarakat99 <judebarakat@yahoo.com>
2025-08-12 23:57:38 +0100

    added a new ws

---
+++ ROS/ros_ws2/src/suture_arm/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'suture_arm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/scenes', ['resource/ur3_suture.ttt']), 
+    ],
+    install_requires=[
+        'setuptools',
+        'ikpy',
+        'numpy',
+        'transforms3d',
+        'coppeliasim-zmqremoteapi-client',
+    ],
+    zip_safe=True,
+    maintainer='mscrobotics2425laptop37',
+    maintainer_email='judebarakat@yahoo.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    #tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'suturing = suture_arm.suture_arm_node:main',
+        ],
+    },
+)
